ecdea56407d56d5fa7c5c78da99bfab8d29c6f61
Alan Jeferson <Alan Jeferson>
2016-09-04 17:59:03 -0300

    Remove trash files

--- build/default/debug/src/extra.p1.d
+++
@@ -1,6 +0,0 @@
- build/default/debug/src/extra.d  \
- build/default/debug/src/extra.p1:  \
- src/extra.c  \
-src/../include/prediction.h  \
-src/../include/defines.h  \
-src/../include/util.h 
--- build/default/debug/src/prediction.p1.d
+++
@@ -1,4 +0,0 @@
- build/default/debug/src/prediction.d  \
- build/default/debug/src/prediction.p1:  \
- src/prediction.c  \
-src/../include/defines.h 
